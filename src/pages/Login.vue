e75268dba23712867667b96ac6d3fa32fd0d2efe
Gumraze-git <galaxydh4110@gmail.com>
2025-05-22 18:12:08 +0900

    feat(login): add axios communication feature and store login information in local storage

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,14 +2,14 @@
   <div class="min-h-screen flex items-center justify-center bg-gray-100">
     <div class="bg-white shadow-md rounded-lg p-8 w-full max-w-md">
       <h2 class="text-2xl font-bold text-gray-800 text-center">Login</h2>
-      <form class="space-y-6 mt-6">
+      <div class="space-y-6 mt-6">
         <!-- Email Input -->
         <div>
           <label for="email" class="block text-sm font-medium text-gray-700">
             Email
           </label>
           <input
-            v-model="email"
+            v-model="member.email"
             id="email"
             type="email"
             placeholder="Enter your email"
@@ -22,7 +22,7 @@
             Password
           </label>
           <input
-            v-model="password"
+            v-model="member.password"
             id="password"
             type="password"
             placeholder="Enter your password"
@@ -32,25 +32,54 @@
         <!-- Submit Button -->
         <div>
           <button
-            type="submit"
+            @click="login"
             class="w-full py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring focus:bg-blue-600"
           >
             Login
           </button>
         </div>
-      </form>
+      </div>
       <p class="mt-4 text-sm text-gray-600 text-center">
         Don't have an account?
-        <a href="/signup" class="text-blue-500 hover:underline">Sign up</a>
+        <router-link class="text-blue-500 hover:underline" to="/signup"
+          >Sign up</router-link
+        >
       </p>
     </div>
   </div>
 </template>
 
 <script setup>
-import axios from 'axios'
-import {ref} from "vue";
+import axios from "axios";
+import { reactive, ref } from "vue";
 
-const email = ref('')
-const password = ref('')
+const member = reactive({
+  email: "",
+  password: "",
+});
+
+const userList = ref([]);
+
+const login = async () => {
+  const response = await axios.get("http://222.117.237.119:8111/users/list");
+
+  userList.value = response.data;
+
+  const matchID = userList.value.filter(
+    (user) => user.email === member.email && user.pwd === member.password
+  );
+
+  console.log(member.email, member.password);
+  console.log(userList.value);
+  console.log(matchID);
+
+  if (matchID) {
+    console.log("회원 정보가 있습니다.");
+    const user = {
+      email: member.email,
+      password: member.password,
+    };
+    localStorage.setItem(user, JSON.stringify(user));
+  }
+};
 </script>
